5598741522c1e74ee969a10ba056eb3736694b97
dal21 <38189650+dal21@users.noreply.github.com>
2021-04-01 17:11:10 +0100

    Second Upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/LovelyAssistant Test 2.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/LovelyAssistant Test 3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
